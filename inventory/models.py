1020502ee21532013f659756abb23648a7661514
MD Salman Mahadi <quantasalman@gmail.com>
2026-07-26 16:37:27 +0200

    Initial Django Clothing Shop Management System

---
+++ inventory/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    brand = models.ForeignKey(
+        Brand,
+        on_delete=models.CASCADE
+    )
+
+    size = models.CharField(max_length=20)
+    color = models.CharField(max_length=50)
+
+    purchase_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    selling_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    quantity = models.IntegerField()
+
+    barcode = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+    created_date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.name
