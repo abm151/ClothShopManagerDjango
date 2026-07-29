b14bfdeda0f03ce889dfcdb25cd88befdd1159f9
MD Salman Mahadi <quantasalman@gmail.com>
2026-07-29 14:50:20 +0200

    Add modern admin dashboard theme

--- ClothShopManagerDjango/settings.py
+++ ClothShopManagerDjango/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -78,11 +80,14 @@ WSGI_APPLICATION = 'ClothShopManagerDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
